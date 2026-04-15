f89fcf6f6ad113f579f1ba56eeba070ee651a24a
Hardik Mishra (H.M.) <87131178+HM245@users.noreply.github.com>
2026-04-15 16:59:11 +0530

    Fix movie asset URLs
    Updated movie URLs to remove leading slash.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,15 +4,15 @@ import '../css/Home.css';
 function Home() {
     const [searchQuery, setSearchQuery] = useState("");
     const movies=[
-        {id: 1, title: "Dhurandhar", release_date: "2025", url: "/src/assets/Dhurandhar.png"},
-        {id: 2, title: "Thama", release_date: "2025", url: "/src/assets/Thama.png"},
-        {id: 3, title: "Kalki", release_date: "2024", url: "/src/assets/Kalki.png"},
-        {id: 4, title: "Rmayana", release_date: "2026", url: "/src/assets/Dhurandhar.png"},
-        {id: 5, title: "King", release_date: "2025", url: "/src/assets/Thama.png"},
-        {id: 6, title: "Bhooth Bangla", release_date: "2024", url: "/src/assets/Kalki.png"},
-        {id: 7, title: "Dhurandhar The Revenge", release_date: "2025", url: "/src/assets/Dhurandhar.png"},
-        {id: 8, title: "Border 2", release_date: "2025", url: "/src/assets/Thama.png"},
-        {id: 9, title: "Drishyam 3", release_date: "2024", url: "/src/assets/Kalki.png"},
+        {id: 1, title: "Dhurandhar", release_date: "2025", url: "src/assets/Dhurandhar.png"},
+        {id: 2, title: "Thama", release_date: "2025", url: "src/assets/Thama.png"},
+        {id: 3, title: "Kalki", release_date: "2024", url: "src/assets/Kalki.png"},
+        {id: 4, title: "Rmayana", release_date: "2026", url: "src/assets/Dhurandhar.png"},
+        {id: 5, title: "King", release_date: "2025", url: "src/assets/Thama.png"},
+        {id: 6, title: "Bhooth Bangla", release_date: "2024", url: "src/assets/Kalki.png"},
+        {id: 7, title: "Dhurandhar The Revenge", release_date: "2025", url: "src/assets/Dhurandhar.png"},
+        {id: 8, title: "Border 2", release_date: "2025", url: "src/assets/Thama.png"},
+        {id: 9, title: "Drishyam 3", release_date: "2024", url: "src/assets/Kalki.png"},
     ];
 
     const handleSearch=(e) => {
