e245733433763bd209ebfcee9bf8acb535e4d3cd
Hardik Mishra (H.M.) <87131178+HM245@users.noreply.github.com>
2026-04-15 16:44:14 +0530

    Change route path

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <NavBar />
       <main className='main-content'>
         <Routes>
-          <Route path='/Home' element={<Home />} />
+          <Route path='/Movies/' element={<Home />} />
           <Route path='/favorites' element={<Favorite />} />
         </Routes>
       </main>
